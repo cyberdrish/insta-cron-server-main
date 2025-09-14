6b69c97af8c8595eadfce519e2cbfabe0d513b5a
cyberdrish <drishmalhotra1997@gmail.com>
2025-09-14 15:57:23 +0530

    env

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-
-const MONGODB_URI = "mongodb://localhost:27017/Testapp";
+require("dotenv").config();
+const MONGODB_URI = process.env.MONGODB_URI;
 
 console.log("URI", MONGODB_URI);
 async function connectToDatabase() {
